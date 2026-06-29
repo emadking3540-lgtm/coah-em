4d1c122c18f1738d7b83fd939ada99e49d586c4b
emadking3540-lgtm <emadking3540@gmail.com>
2026-06-30 02:40:06 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
   defaultConfig {
     applicationId = "com.aistudio.gymzone.axrtmp"
-    minSdk = 24
+    minSdk = 21
     targetSdk = 36
     versionCode = 1
     versionName = "1.0"
